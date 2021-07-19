19f2f9d0cc321862700d8990c6b617361181fdb6
SairajK19 <saikapdi11@gmail.com>
2021-07-19 19:12:20 +0530

    added update paper mapping

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt, Value } from "@graphprotocol/graph-ts"
+import { BigInt, Value } from "@graphprotocol/graph-ts";
 import {
   Contract,
   Approval,
@@ -6,74 +6,26 @@ import {
   Funding,
   Published,
   Transfer,
-  UpdatePaper
-} from "../generated/Contract/Contract"
+  UpdatePaper,
+} from "../generated/Contract/Contract";
 
-// import { ExampleEntity } from "../generated/schema"
 import { Paper } from "../generated/schema";
-
-// export function handleApproval(event: Approval): void {
-//   // Entities can be loaded from the store using a string ID; this ID
-//   // needs to be unique across all entities of the same type
-//   let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-//   // Entities only exist after they have been saved to the store;
-//   // `null` checks allow to create entities on demand
-//   if (entity == null) {
-//     entity = new ExampleEntity(event.transaction.from.toHex())
-
-//     // Entity fields can be set using simple assignments
-//     entity.count = BigInt.fromI32(0)
-//   }
-
-//   // BigInt and BigDecimal math are supported
-//   entity.count = entity.count + BigInt.fromI32(1)
-
-//   // Entity fields can be set based on event parameters
-//   entity.owner = event.params.owner
-//   entity.approved = event.params.approved
-
-//   // Entities can be written to the store with `.save()`
-//   entity.save()
-
-//   // Note: If a handler doesn't require existing field values, it is faster
-//   // _not_ to load the entity from the store. Instead, create it fresh with
-//   // `new Entity(...)`, set the fields that should be updated and save the
-//   // entity back to the store. Fields that were not set or unset remain
-//   // unchanged, allowing for partial updates to be applied.
-
-//   // It is also possible to access smart contracts from mappings. For
-//   // example, the contract that has emitted the event can be connected to
-//   // with:
-//   //
-//   // let contract = Contract.bind(event.address)
-//   //
-//   // The following functions can then be called on this contract to access
-//   // state variables and other data:
-//   //
-//   // - contract.balanceOf(...)
-//   // - contract.donations(...)
-//   // - contract.getApproved(...)
-//   // - contract.isApprovedForAll(...)
-//   // - contract.name(...)
-//   // - contract.ownerOf(...)
-//   // - contract.papers(...)
-//   // - contract.supportsInterface(...)
-//   // - contract.symbol(...)
-//   // - contract.tokenURI(...)
-//   // - contract.getPaper(...)
-// }
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
-export function handleFunding(event: Funding): void {}
+export function handleFunding(event: Funding): void {
+}
 
 export function handlePublished(event: Published): void {
+  // create a new instance of the paper.
   let paper = new Paper(event.params.tokenId.toHex());
 
+  // update with new parameters.
   paper.owner = event.params.owner;
   paper.tokenUri = event.params.tokenUri;
-  (event.params.allowFunding == true)? paper.allowFunding = "true": paper.allowFunding = "false";
+  event.params.allowFunding == true
+    ? (paper.allowFunding = "true")
+    : (paper.allowFunding = "false");
   paper.author = event.params.author;
   paper.fundAmount = event.params.fundAmount.toString();
   paper.totalAmountFunded = "0";
@@ -83,4 +35,17 @@ export function handlePublished(event: Published): void {
 
 export function handleTransfer(event: Transfer): void {}
 
-export function handleUpdatePaper(event: UpdatePaper): void {}
+export function handleUpdatePaper(event: UpdatePaper): void {
+  // get the paper id.
+  let id = event.params.paperid.toHex();
+  // get the paper.
+  let paper = Paper.load(id);
+
+  // update paper with new parameters.
+  paper.fundAmount = event.params.amount.toString();
+  event.params.allowFunding == true
+    ? (paper.allowFunding = "true")
+    : (paper.allowFunding = "false");
+
+  paper.save();
+}
